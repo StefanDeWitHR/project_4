b65eff921a7ae4cef0c7d42be59f1e286eb00af0
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-06-26 14:27:40 +0200

    Design patterns added

---
+++ WindowsFormsApp/SharedLibrary/Logic/Locations.cs
@@ -0,0 +1,35 @@
+﻿using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+   
+    public class Locations<T>
+    {
+        Location loc;
+        public Locations(Location loc)
+        {
+            this.loc = loc;
+        }
+        public void SetLocation(string x_coord , string y_coord)
+        {
+            
+        }
+        public Option<Location> GetLocation(int education_id)
+        {
+            var LinqQuery = (from l in locations
+                             select l).ToList();
+            return new Some<Location>(LinqQuery);
+        }
+        
+        public List<Option<Location>> GetLocations()
+        {
+           // return new Some<Location>(List<Location>());
+        }
+    }
+
+}
