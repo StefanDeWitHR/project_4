7ea13fa0a0971c2c2c80c6a3b14a1105a66b2343
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-06-26 11:24:13 +0200

    Fixed project

--- WindowsFormsApp/Android_app/Android_app/ViewModels/BaseViewModel.cs
+++ WindowsFormsApp/Android_app/Android_app/ViewModels/BaseViewModel.cs
@@ -6,18 +6,18 @@ using Xamarin.Forms;
 
 namespace Android_app.ViewModels
 {
-	public class BaseViewModel : ObservableObject
+	public class BaseViewModel
 	{
 		/// <summary>
 		/// Get the azure service instance
 		/// </summary>
-		public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+		
 
 		bool isBusy = false;
 		public bool IsBusy
 		{
 			get { return isBusy; }
-			set { SetProperty(ref isBusy, value); }
+			
 		}
 		/// <summary>
 		/// Private backing field to hold the title
@@ -29,7 +29,7 @@ namespace Android_app.ViewModels
 		public string Title
 		{
 			get { return title; }
-			set { SetProperty(ref title, value); }
+			
 		}
 	}
 }
