9bb5fee908a4d32800efc21fdec5431d484f50b0
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-06-26 14:19:47 +0200

    NEW

---
+++ App1/App1/App1.iOS/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using App1;
+
+namespace App1.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+
+            //var something = new App1.Class1();
+           // something.IRule
+        }
+    }
+}
